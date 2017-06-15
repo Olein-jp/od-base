c6b533481241b6c91c8be92e149f54008d3c6492
KojiKuno <koji.kuno@gmail.com>
2017-06-15 13:21:16 +0900

    make site-footer

--- index.php
+++ index.php
@@ -81,6 +81,14 @@
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	</aside>
 </div>
+<footer class="site-footer">
+	<div class="site-footer__inner">
+		
+	</div>
+	<div class="copyright">
+		<p>©Copyright <a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'title' ); ?></a><?php if ( get_bloginfo( 'description' ) ): ?> | <?php bloginfo( 'description' ); endif; ?>. All Rights Reserved.</p>
+	</div>
+</footer>
 </div>
 <?php wp_footer(); ?>
 </body>
