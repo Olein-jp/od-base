018a5a99825370a8704a784d02f69ee2f4d6d41e
KojiKuno <koji.kuno@gmail.com>
2017-06-16 10:12:35 +0900

    changed wrapping tag to main tag

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  */
 ?>
 <?php get_header(); ?>
-	<div id="primary" class="content-area">
+	<main id="primary" class="content-area">
 		<?php if ( have_posts() ) : while ( have_posts() ): the_post(); ?>
 		<?php
 			// カテゴリー名をリンクなしで取得したい場合
@@ -41,6 +41,6 @@
 			</div>
 		</article>
 		<?php endwhile; endif; ?>
-	</div>
+	</main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
