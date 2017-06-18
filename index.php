d1bbcb9506e2becac3e1a38f9ac50340a07d23d8
KojiKuno <koji.kuno@gmail.com>
2017-06-18 16:57:42 +0900

    added entry header

--- index.php
+++ index.php
@@ -15,27 +15,29 @@
 			// 出力はline.23
 		?>
 		<article id="post-<?php the_id(); ?>" <?php post_class(); ?>>
-			<h2 class="entry-title">
-				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-			</h2>
-			<div class="entry-meta">
-				<span class="date">投稿日 : <?php the_time( get_option( 'date_format' ) ); ?></span>
-				<span class="category">カテゴリー : <?php if ( $cat ) { echo esc_html( $cat->name ); } // カテゴリー名（リンクなし）を表示 ?></span>
-				<span class="categori-links">カテゴリー : <?php the_category( '、' ); ?></span>
-				<span class="tag-links">タグ : <?php the_tags( '', '、' ); ?></span>
-				<span class="author">投稿者 : <?php the_author(); ?></span>
-			</div>
-			<?php if ( has_post_thumbnail() ): ?>
-			<figure class="entry-thumbnail">
-				<?php the_post_thumbnail(); ?>
-			</figure>
-			<?php 
-				/* else:
-				 * サムネイルがない場合に挿入する画像を指定
-				 * その際にはfigureの位置を再考せよ
-				*/
-			?>
-			<?php endif; ?>
+			<header class="entry-header">
+				<h2 class="entry-title">
+					<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+				</h2>
+				<div class="entry-meta">
+					<span class="date">投稿日 : <?php the_time( get_option( 'date_format' ) ); ?></span>
+					<span class="category">カテゴリー : <?php if ( $cat ) { echo esc_html( $cat->name ); } // カテゴリー名（リンクなし）を表示 ?></span>
+					<span class="categori-links">カテゴリー : <?php the_category( '、' ); ?></span>
+					<span class="tag-links">タグ : <?php the_tags( '', '、' ); ?></span>
+					<span class="author">投稿者 : <?php the_author(); ?></span>
+				</div>
+				<?php if ( has_post_thumbnail() ): ?>
+				<figure class="entry-thumbnail">
+					<?php the_post_thumbnail(); ?>
+				</figure>
+				<?php 
+					/* else:
+					 * サムネイルがない場合に挿入する画像を指定
+					 * その際にはfigureの位置を再考せよ
+					*/
+				?>
+				<?php endif; ?>
+			</header>
 			<div class="entry-excerpt">
 				<?php the_excerpt(); ?>
 			</div>
